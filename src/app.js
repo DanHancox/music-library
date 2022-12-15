8c61cbebee15a1d9760b95e699973687e27b0dae
Dan Hancox <dthancox@gmail.com>
2022-12-15 10:44:51 +0000

    first test passed

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
-const express = require('express');
-const app = express();
-app.use = (express.json());
 
-app.get('/', (_req ,res) => {
-    res.send('Hello World');
-});
+const express = require('express');
+const artistRouter = require('./routes/artist');
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/artist', artistRouter);
+
+
 
 
 
